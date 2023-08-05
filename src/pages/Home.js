59183e05a8f3e84af120835bc4b1885f2b53cf6e
Przemysław Bąbel <86799399+BabelWebDeveloper@users.noreply.github.com>
2023-08-05 16:56:20 +0200

    Add warning message about prevent updaing and deleting records

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,60 +1,68 @@
-import supabase from '../config/supabaseClient'
-import { useEffect, useState } from 'react'
+import supabase from "../config/supabaseClient";
+import { useEffect, useState } from "react";
 
 // components
-import SmoothieCard from '../components/SmoothieCard'
+import SmoothieCard from "../components/SmoothieCard";
 
 const Home = () => {
-  const [fetchError, setFetchError] = useState(null)
-  const [smoothies, setSmoothies] = useState(null)
-  const [orderBy, setOrderBy] = useState('created_at')
+  const [fetchError, setFetchError] = useState(null);
+  const [smoothies, setSmoothies] = useState(null);
+  const [orderBy, setOrderBy] = useState("created_at");
 
   const handleDelete = (id) => {
-    setSmoothies(prevSmoothies => {
-      return prevSmoothies.filter(sm => sm.id !== id)
-    })
-  }
+    setSmoothies((prevSmoothies) => {
+      return prevSmoothies.filter((sm) => sm.id !== id);
+    });
+  };
 
   useEffect(() => {
     const fetchSmoothies = async () => {
       const { data, error } = await supabase
-        .from('Smoothies')
+        .from("Smoothies")
         .select()
-        .order(orderBy, {ascending: false})
-      
+        .order(orderBy, { ascending: false });
+
       if (error) {
-        setFetchError('Could not fetch the smoothies')
-        setSmoothies(null)
+        setFetchError("Could not fetch the smoothies");
+        setSmoothies(null);
       } else {
-        setSmoothies(data)
-        setFetchError(null)
+        setSmoothies(data);
+        setFetchError(null);
       }
-    }
+    };
 
-    fetchSmoothies()
-
-  }, [orderBy])
+    fetchSmoothies();
+  }, [orderBy]);
 
   return (
     <div className="page home">
-      {fetchError && (<p>{fetchError}</p>)}
+      {fetchError && <p>{fetchError}</p>}
       {smoothies && (
         <div className="smoothies">
           <div className="order-by">
             <p>Order by:</p>
-            <button onClick={() => setOrderBy('created_at')}>Time Created</button>
-            <button onClick={() => setOrderBy('title')}>Title</button>
-            <button onClick={() => setOrderBy('rating')}>Rating</button>
+            <button onClick={() => setOrderBy("created_at")}>
+              Time Created
+            </button>
+            <button onClick={() => setOrderBy("title")}>Title</button>
+            <button onClick={() => setOrderBy("rating")}>Rating</button>
           </div>
           <div className="smoothie-grid">
-            {smoothies.map(smoothie => (
-              <SmoothieCard key={smoothie.id} smoothie={smoothie} onDelete={handleDelete} />
+            {smoothies.map((smoothie) => (
+              <SmoothieCard
+                key={smoothie.id}
+                smoothie={smoothie}
+                onDelete={handleDelete}
+              />
             ))}
+          </div>
+          <div class="alert alert-warning" role="alert">
+            Delete and update features are locked for guest users!
           </div>
         </div>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
